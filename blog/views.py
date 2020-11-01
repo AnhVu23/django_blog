9043806fd8e60d7b601f19db12bcd33275cf5bb3
Anh Vu <vu.haianh291@gmail.com>
2020-11-01 23:12:47 +0200

    Reformat date

--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-posts = [
-    {
-        'author': 'Anh Vu',
-        'title': 'Introduction to React Native',
-        'content': 'React Native is a mobile cross-platform library',
-        'date_posted': 'October 28, 2020'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Advanced React Native',
-        'content': 'React Native can run the app in 60fps',
-        'date_posted': 'October 28, 2020'
-    },
-    {
-        'author': 'John Smith',
-        'title': 'Introduction to React',
-        'content': 'React is made by Facebook',
-        'date_posted': 'October 28, 2020'
-    }
-]
 def home(request):
     context = {
-        'posts': posts,
+        'posts': Post.objects.all(),
     }
     return render(request, 'blog/home.html', context)
 
